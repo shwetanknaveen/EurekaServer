d82235239e2176d56109c3375070f1b8a39df636
shwetanknaveen <shwetanknaveen@gmail.com>
2022-11-30 11:14:11 +0530

    working eureka server with right version of Java

---
+++ src/main/java/com/exam/architecturefinal/EurekaServer/EurekaServerApplication.java
@@ -0,0 +1,24 @@
+package com.exam.architecturefinal.EurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getTemplate() {
+        return new RestTemplate();
+    }
+}
